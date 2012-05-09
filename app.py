f6c7aecc733ef83205bd82673402ca23932dfca8
Jack Pearkes <jackpearkes+git@gmail.com>
2012-05-08 22:58:52 -0700

    upload endpoint, countdown and frontend upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, session, flash, render_template, abort
+from flask import Flask, request, redirect, url_for, session, flash, render_template, abort, jsonify
 from flaskext.sqlalchemy import SQLAlchemy
 from flaskext.cache import Cache
 from flaskext.wtf import Form, TextField
@@ -15,7 +15,7 @@ assets.register('css',
                 output='assetcache/cached.css', filters='cssmin')
 
 assets.register('js',
-                'js/jquery.ui.widget.js', 'js/jquery.iframe-transport.js', 'js/jquery.fileupload.js',
+                'js/jquery.ui.widget.js', 'js/jquery.iframe-transport.js', 'js/jquery.fileupload.js', 'js/jquery.countdown.min.js',
                 output='assetcache/cached.js', filters='jsmin')
 
 @app.route('/')
@@ -26,6 +26,14 @@ def index():
 def song(id):
 	print id
 	return render_template('song.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        data_file = request.files.get('file')
+        file_name = data_file.filename
+        print file_name
+    return jsonify(name=file_name, size=15151, url="\/\/example.org\/files\/picture1.jpg")
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
